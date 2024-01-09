e4e0f1356ef936b3d5d7afd1711977ec4951832c
Neill Robson <neill.robson@pendo.io>
2024-01-09 10:32:30 -0500

    Initial pass at data gen util

---
+++ src/utils/generate.js
@@ -0,0 +1,49 @@
+const aCode = 97;
+
+function alphaIncrement(str) {
+    let carry = true;
+
+    let result = str.split('').reduceRight((acc, c) => {
+        let code = c.charCodeAt(0);
+        code -= aCode;
+        code = (code + (carry ? 1 : 0)) % 26;
+        code += aCode;
+        carry = code === aCode;
+        acc.unshift(String.fromCharCode(code));
+
+        return acc;
+    }, []);
+
+    if (carry) {
+        result.unshift('a');
+    }
+
+    return result.join('');
+}
+
+function* alphabetKeys() {
+    let string = '';
+
+    while (true) {
+        string = alphaIncrement(string);
+
+        yield string;
+    }
+}
+
+/**
+ * Create a dummy object with the given number of keys (lowercase excel column names)
+ *
+ * For reference, the default number of keys is 1 million, creating keys from 'a' to 'bdwgo'
+ *
+ * @param {Number} count Number of keys to generate
+ * @param {Function} filler Function used to generate values
+ */
+export function alphabetMap(count = 1e6, filler = (i) => ({ i })) {
+    const ret = {};
+    const itr = alphabetKeys();
+
+    for (let i = 0; i < count; i++) {
+        ret[itr.next().value] = filler(i);
+    }
+}
